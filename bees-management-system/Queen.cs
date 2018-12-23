f1989e91098c69ead8107dd8b8c0b4444992e6d4
kpilszak <klaudia.pilszak@gmail.com>
2018-12-23 13:18:34 +0100

    Update Queen.cs

--- bees-management-system/Queen.cs
+++ bees-management-system/Queen.cs
@@ -1,12 +1,40 @@
 ﻿namespace bees_management_system
 {
-    class Queen
+    public class Queen
     {
-        private Worker[] workers;
-
         public Queen(Worker[] workers)
         {
             this.workers = workers;
+        }
+
+        private Worker[] workers;
+        private int shiftNumber = 0;
+
+        public bool AssignWork(string job, int numberOfShifts)
+        {
+            for (int i = 0; i < workers.Length; i++)
+                if (workers[i].DoThisJob(job, numberOfShifts))
+                    return true;
+            return false;
+        }
+
+        public string WorkTheNextShift()
+        {
+            shiftNumber++;
+            string report = "Shift report number " + shiftNumber + "\n";
+            for (int i = 0; i < workers.Length; i++)
+            {
+                if (workers[i].DidYouFinish())
+                    report += "Bee number " + (i + 1) + " has just finished its job\r\n";
+                if (string.IsNullOrEmpty(workers[i].CurrentJob))
+                    report += "Bee number " + (i + 1) + " isn't working\r\n";
+                else
+                    if (workers[i].ShiftsLeft > 0)
+                    report += "Bee number " + (i + 1) + " is working. '" + workers[i].CurrentJob + "' will be her job for " + workers[i].ShiftsLeft + " more shifts\r\n";
+                else
+                    report += "Bee number " + (i + 1) + " is about to finish the job. '" + workers[i].CurrentJob + "' will be done after this shift\r\n"; 
+            }
+            return report;
         }
     }
 }
